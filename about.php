3a6f220e5654bf58699b3d32215d9ca7ffa6f55c
Manju <gowdamanjunath195@gmail.com>
2023-09-20 20:12:34 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,211 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About Us</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>We are convinced that delivering a completely satisfying home selling and buying experience requires proactiveness and an ability to maintain solid communications throughout the transaction process.</p>
+         <a href="contact.html" class="inline-btn">contact us</a>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="heading">3 simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/step-1.png" alt="">
+         <h3>search property</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/step-2.png" alt="">
+         <h3>contact agents</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/step-3.png" alt="">
+         <h3>enjoy property</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- review section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-1.png" alt="">
+            <div>
+               <h3>Ram</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Finding and buying a house was not an easy task. RealEstateIndia made it possible in a very easy way. The agent I was been provided was so down to earth and soft spoken.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-2.png" alt="">
+            <div>
+               <h3>Angel Priya</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Buy Commercial Property any where in India you need a Real Estate Consultant that is quick and has ample knowledge of real estate trends.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-3.png" alt="">
+            <div>
+               <h3>berlin</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>I find Realestateindia best for this work. They help me to get Commercial Property in a reasonable price</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-4.png" alt="">
+            <div>
+               <h3>Sita</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>I am a software engineer and recently got placed in Bangalore. I decided not to shell out huge sums on a monthly basis in giving rent to proprietors.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-5.png" alt="">
+            <div>
+               <h3>shashank</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>The price they offered was quite affordable. Thanks to realestateIndia.com, I own a beautiful home in a good society today.</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-6.png" alt="">
+            <div>
+               <h3>Shakila</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>The Property related service from Realestateindia are my source of guidance in all advanced and complex property related problems.!</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- review section ends -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
